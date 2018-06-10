101646b562e7504cc552693577aa9e3acfd8dd49
Micha-Btz <github@mdomann.tk>
2018-06-10 20:21:35 +0200

    set path to lineage
    Change-Id: I2fd6ccd03b622f8e97e81185d7a1318e5e84b521

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/amami/full_amami.mk)
 
